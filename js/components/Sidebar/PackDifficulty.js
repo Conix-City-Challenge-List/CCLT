67ef9c74aabdc33cf8db77c935e5c6254b2f9332
Duckly <76588336+FudezPal@users.noreply.github.com>
2025-12-25 18:58:15 +0200

    Update PackDifficulty.js

--- js/components/Sidebar/PackDifficulty.js
+++ js/components/Sidebar/PackDifficulty.js
@@ -3,22 +3,22 @@ export default {
         <h3>Difficulty Rankings</h3>
         <div class="right-text">
             <p>
-                SILENT Level Pack = Most of These levels have not even been verified before (200 points)
+                SILENT Level Pack = Most of These levels have not even been verified before (1000 points)
             </p>
             <p>
-                @Impossible level pack = These challenges are HELL! (150 points)
+                Impossible level pack = These challenges are HELL! (750points)
             </p>
             <p>
-                Extreme level pack = These challenges are HARD! (100 points)
+                Extreme level pack = These challenges are HARD! (500 points)
             </p>
             <p>
-                Hard level pack = Goodluck.. (70 points)
+                Hard level pack = Goodluck.. (300 points)
             </p>
             <p>
-                Medium level pack = Packs with levels from the medium tier (30 points)
+                Medium level pack = Packs with levels from the medium tier (200 points)
             </p>
             <p>
-                Easy level pack = Easy! (15 points)
+                Easy level pack = Easy! (100 points)
             </p>
         </div>
     `
