0705347166b8e35ca9e2d3a610b77cca596fa3e0
Duckly <76588336+FudezPal@users.noreply.github.com>
2025-12-25 05:14:00 +0200

    Update PacksInfo.js

--- js/components/Sidebar/PacksInfo.js
+++ js/components/Sidebar/PacksInfo.js
@@ -1,10 +1,9 @@
 export default {
     template: `
-        <h3>About Packs</h3>
+        <h3>About Conix Packs</h3>
         <div class="right-text">
-            <p>Packs are sets of levels on the Layout List chosen by the staff team that share distinct commonalities and are within a close difficulty range (generally ± 1 difficulty tier, with the exception of very specific and/or joke packs).</p>
-            <p>If you have a suggestion for a new pack, feel free to share it with the list team in #list-discussion in our Discord server!</p>
-            <p>If you beat all the levels in a pack, it gets displayed on your profile in the leaderboard!  Furthermore, you can send a screenshot of your list profile in #list-support in our Discord server, and we will give you the roles for the packs you've completed!</p>
+            <p>Packs are sets of levels that conix has made. They range from simple easy challenges to some of the hardest challenges you will ever see!).</p>
+            <p>If you beat all the levels in a pack, it gets displayed on your profile in the leaderboard!  Furthermore, you can send a screenshot of your list profile in our Discord server, and we will give you the roles for the packs you've completed!</p>
         </div>
     `
 }
