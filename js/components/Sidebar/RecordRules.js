424388d7c5e546f8473ba808680dab0d022be3e9
Duckly <76588336+FudezPal@users.noreply.github.com>
2025-12-25 05:44:36 +0200

    Update RecordRules.js

--- js/components/Sidebar/RecordRules.js
+++ js/components/Sidebar/RecordRules.js
@@ -3,13 +3,13 @@ export default {
         <h3>Record Submission Requirements</h3>
         <div class="right-text">
             <p>
-                You must have achieved the record without using hacks (including hacks that change the physics of the game, ie. physics bypass via MegaHack. Note that Click Between Frames is allowed but its Physics Bypass setting is banned.).
+                You must have achieved the record without using hacks (including hacks that change the physics of the game, ie. physics bypass via MegaHack. Note that Click Between Frames is allowed but Physics Bypass is banned.).
             </p>
             <p>
-                You must have achieved the record on the level that is listed on the site or on an approved bugfixed copy - please check the level ID before you submit a record!
+                Level MUST be less than 30 seconds long. No longer. If It is longer than 30 seconds it will not be accepted on the list.
             </p>
             <p>
-                Records for Easy+ completions must have clicks or visual tap indication (source audio is acceptable for iPhone users). This does not include mods that add artificial click sounds.
+                Completion must have clicks clearly audible in the video. (Mobile players are exempt from this rule but they should preferably have taps.)
             </p>
             <p>
                 Complete raw footage is required alongside your record for any layouts in Extreme Tier or above.
@@ -18,14 +18,13 @@ export default {
                 The recording must have a previous attempt and death animation shown before the completion, unless the completion is on the first attempt.
             </p>
             <p>
-                The recording must show the player hit the end-wall as well as present your end stats, or the completion will be invalidated.
+                The recording must show the player hit the endwall and the endscreen must be shown or the completion will be invalidated.
             </p>
             <p>
                 Do not use secret routes, skips, or bug routes!
             </p>
             <p>
-                Cheat Indicator is required for all completions via MegaHack, Geode, or iCreate Pro. If you do not have Cheat Indicator on, your record will likely be invalidated (this is not 100% required for mobile as of yet due to mobile limitations).
-                If you are unsure which Geode mod menu to use, we recommend Eclipse.
+                Cheat Indicator is required for all completions via MegaHack, Geode, or iCreate Pro. If you do not have Cheat Indicator on, your record will likely be invalidated. (This is not 100% required for mobile as of yet due to mobile limitations).
             </p>
         </div>
     `
